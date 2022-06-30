845dd3c69d75a194511dfefaadb91432f17159ae
Amar Pan, M.Ed <89368298+profpan396@users.noreply.github.com>
2022-06-30 05:38:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*
+File Name: main.java
+Developer: Amar Panjwani
+Date last modified: 2/21/21
+Description: Graphic of a house using ASCII characters
+email address: panjwania@student.vvc.edu
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("        +"); 
+    System.out.println("     +     +");
+    System.out.println("  +           +");
+    System.out.println("+---------------+");
+    System.out.println("|      .-.      |");
+    System.out.println("|      | |      |");
+    System.out.println("+-+-+-+-+-+-+-+-+");
+  }
+}
